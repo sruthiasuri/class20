bb63f93a3fd4dcf2977d4a0f16f9f9005de18ceb
sruthiasuri <69282095+sruthiasuri@users.noreply.github.com>
2020-10-11 07:17:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+
+var fixedRect,movingRect;
+function setup() {
+  createCanvas(1200,800);
+fixedRect=createSprite(600,200,50,80);
+fixedRect.shapeColor="green";
+fixedRect.debug=true;
+
+movingRect=createSprite(400,200,80,30);
+movingRect.shapeColor="green";
+movingRect.debug="true";
+  
+}
+
+function draw() {
+  background("black");  
+  movingRect.x=World.mouseX;
+  movingRect.y=World.mouseY;
+  if(movingRect.x-fixedRect.x<movingRect.width/2+fixedRect.width/2&&
+    fixedRect.x-movingRect.x<movingRect.width/2+fixedRect.width/2)
+  {
+    movingRect.shapeColor="red";
+    fixedRect.shapeColor="red";
+  }
+  else{
+    movingRect.shapeColor="green";
+    fixedRect.shapeColor="green";
+  }
+  drawSprites();
+}
